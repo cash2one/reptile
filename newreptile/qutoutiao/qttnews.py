f784d2d50a68b2f3b8dac68420c78c23e2bf9ec4
shucheng.qu <917361050@qq.com>
2018-10-12 10:05:01 +0800

    qtt news

--- newreptile/qutoutiao/qttnews.py
+++ newreptile/qutoutiao/qttnews.py
@@ -3,6 +3,7 @@
 #   @file: qttnews.py
 #   @Created by shucheng.qu on 2018/10/10
 import json
+import time
 
 import requests
 from lxml import etree
@@ -11,7 +12,7 @@ from newreptile.utils.dirs import makedirs
 
 url = 'https://api.1sapp.com/content/getListV2?qdata=RUFEODFGQjBBMkY5RjRGRUFBMDk5RjRFREE4RTI5QzMuY0dGeVlXMGZSRGRHUmtJMk1EUXRRekJDUkMwME9EQkJMVUl4UWpFdE9FWTJSREJCUVVZNE9EUXpIblpsY25OcGIyNGZNaDV3YkdGMFptOXliUjlwYjNNPS6cnQPTmo4UjSjjHVUWEz/dv5YPSy4rvc5iVhH2MBc3pKjjm4xOhe%2BgA1WBIyklwnPT6gi/emFarE11slbxnOkHkOW5DX8mTQy/eHYWOOJXnp7NUMXJF98HmYgwIxEljt/Z%2Bp365yCqX6SNm2LjaX9XmY31PbTDecLycr6bMZORw9byqtBWd1CVkuDZMkE%2BArcUZ3VxpdRXphg226jAMwTHcZp34xNoWv6dbcO7j3YubTFikhe%2BT6qKbZn1rBtmSaQt0NaTsogwazSmGPrVZu8XnhCA5JZE%2BtmMVToMCyF3D6WRm9l4IKy0Vo%2BI4QxGQZPcTvitJJM1pOZaFDYnHp6wPOyQbaRNFKHsharQLEcrc17jCEakYKGO3e1O3dipFi9jANedWJRrapsSosxq0Rv/e7e8P4faDpRzdmRg3xviyG1XhUbGm0WhD2wV8Jw7VEfLRAsIJ9s%2BnYCjmeWyftq%2BMgYmULu5g/5dSeiO47PmOOZiDzx6qEhYCfZjnwRnU6S6igzmG1bV24Ag5CsWwb8IRWkojNMbmE0POK/fPg8hHUrgq5xoq%2B5sZ1xWogbd8UajX/G8b6ubrKQjdn/7hWeSzvWwiSTLWMUVXYHOmOP9uDs8nxpPgkFjQjMHqMcQdMeZuRxAlo3Tmb5LbPFZ037kWpPmTankZMRdjCrm1%2BjT7HkroQd0ZKc%2BjJ6NCDRaeNLR2j8cwsIGh1qKNJVs3YSNhTH2Mq4h0pf6%2BiJIshacRr7MvvzxPy%2BwWH8pVhk/nOtoaAEOF49V0fWvSoVK7uPN2uVtpXRsaxXCCBmU9YDfw/o%3D'
 
-for index in range(1,1000):
+for index in range(1, 100):
     content = requests.get(url).content.decode('utf-8')
     datas = json.loads(content)['data']['data']
     print(content)
@@ -32,21 +33,21 @@ for index in range(1,1000):
             news_content = requests.get(share_url).content.decode('utf-8')
             divs = etree.HTML(news_content).xpath('/html/body/section[1]/div/div[2]/p')
             path = makedirs('news', 'qtt')
-            with open(F'{path}/{title}.txt','w') as txt:
+            with open(F'{path}/{title}.txt', 'w') as txt:
                 txt.write('')
-            with open(F'{path}/{title}.txt','a') as txt:
+            with open(F'{path}/{title}.txt', 'a') as txt:
                 txt.write(F'title:{title}\n')
                 txt.write(F'introduction:{introduction}\n')
                 for div in divs:
                     text = div.xpath('./text()')
                     src = div.xpath('./img/@data-src')
-                    if len(text) >0:
+                    if len(text) > 0:
                         txt.write(text[0])
                         txt.write('\n')
-                    if len(src) >0:
+                    if len(src) > 0:
                         txt.write(src[0])
                         txt.write('\n')
         except Exception as e:
             print(e)
-    break
-
+    time.sleeep(5)
+    print(F'趣头条 新闻爬虫 {index}    页完成！')
